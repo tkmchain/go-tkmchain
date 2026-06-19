0f1f01512b6588c7f905b4eaca5f0a07803d4785
tkm <260807532+71mengr@users.noreply.github.com>
2026-06-19 14:41:28 +0000

    Added enr

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -22,6 +22,7 @@ import "github.com/ethereum/go-ethereum/common"
 // the main RandomX network.
 var MainnetBootnodes = []string{
 	// Ethereum Foundation Go Bootnodes
+        "enode://cc01b3e649c0d1e6a8fc3a8037552237d2f778d29efb798b66537a6112e2343ecbf0ae95441b73d4590f1286aa32d30b4dfa2755e7692618e6053595c4975f57@187.124.217.73:3000",
         "enode://2c36e766ab52f04abfc129891b0d92d4d61dff6b8cf496910fd7046be7ca66afddc0086d527d9540003e766716a5337a2b866f8519708996fb8ff645e0b6b52e@129.151.164.202:3000",
         "enode://11533a81e1c318f91ba257bc6e4dec8476b7f70f4534280ce8a4107ba0d31e4613d285d2fa5712aa39741c41ee9f7eaecc759a4b3aea6825c50c8dae9174cb45@129.151.164.223:3000",
 }
@@ -45,7 +46,7 @@ var (
 )
 
 var V5Bootnodes = []string{
-//        "enr:-J24QNptZgMENguLj2GqIz5grYg6PG43Gp6zAJG3tg2kyEwvaboCh085uKL799yLWxt6ouU2en0YarV-MHH9ZqMgMnWGAZ7ZjOpRg2V0aMfGhBMEcTCAgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQIsNudmq1LwSr_BKYkbDZLU1h3_a4z0lpEP1wRr58pmr4N0Y3CCC7iDdWRwggu4",
+        "enr:-J24QDURbVyt4rhioV0Fc4Fhs2n8ihxCSsZ0G39VUb6qOTvqP-3rCAdzAlvMwc5s2LG_-RQW5XjHWUFyEFtOAwTCQ_CGAZ7gNIyeg2V0aMfGhBMEcTCAgmlkgnY0gmlwhLt82UmJc2VjcDI1NmsxoQPMAbPmScDR5qj8OoA3VSI30vd40p77eYtmU3phEuI0PoN0Y3CCC7iDdWRwggu4",
 	"enr:-KO4QA-YD4j8Po5DdSDRJpfCfJmDo0eM-SwreDC6lKQi4KZ-JN0NTVyihf-g706wtLQ6up7EDWJlUXndqEiC3VR_EH2GAZ5BUnWUg2V0aMfGhJa7DLOAgmlkgnY0gmlwhIGXpMqJc2VjcDI1NmsxoQLjIoVsk8G4o5W5DuW_imiRJbusRl0XaVD5fI8UgSeFs4RzbmFwwIN0Y3CCC7iDdWRwggu4",
         "enr:-KO4QKaE8k2YQewzHbQbVoBxCTzTf0_VTBndEaahyw25y2GqdYcHBEn_h9mHtxBx2udDPQhxk6tVzUYiu_ZlxgAQMSmGAZ5BXkANg2V0aMfGhJa7DLOAgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQPQEKDyQnRC0i52hxhsHFYDKUmCSxudn4H-WpGA6L7HhoRzbmFwwIN0Y3CCC7iDdWRwggu4",
 }
