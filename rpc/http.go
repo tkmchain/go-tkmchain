684e246ab7a227e0242220f3702c88502cac6a00
tkm <260807532+71mengr@users.noreply.github.com>
2026-09-02 13:37:08 +0000

    rpc: allow slow keystore export requests

--- rpc/http.go
+++ rpc/http.go
@@ -117,9 +117,9 @@ type HTTPTimeouts struct {
 // DefaultHTTPTimeouts represents the default timeout values used if further
 // configuration is not provided.
 var DefaultHTTPTimeouts = HTTPTimeouts{
-	ReadTimeout:       30 * time.Second,
+	ReadTimeout:       120 * time.Second,
 	ReadHeaderTimeout: 30 * time.Second,
-	WriteTimeout:      30 * time.Second,
+	WriteTimeout:      120 * time.Second,
 	IdleTimeout:       120 * time.Second,
 }
 
